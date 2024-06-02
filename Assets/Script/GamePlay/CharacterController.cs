eadd79e035b1c3c95fc81e0ff4b23bf769fef20c
billwillman <billwillman@qq.com>
2024-06-03 01:47:42 +0800

    22

--- Assets/Script/GamePlay/CharacterController.cs
+++ Assets/Script/GamePlay/CharacterController.cs
@@ -5,6 +5,7 @@ using Unity.Netcode;
 
 namespace SOC.GamePlay
 {
+    // 加上这个可以在LUA覆写方法
     [XLua.LuaCallCSharp]
     public class MoeCharacterController : NetworkBehaviour
     { }
--- Assets/Script/GamePlay/Player/PlayerController.cs
+++ Assets/Script/GamePlay/Player/PlayerController.cs
@@ -5,6 +5,7 @@ using Unity.Netcode;
 
 namespace SOC.GamePlay.Player
 {
+    // 加上这个可以在LUA覆写方法
     // 用来做和DS做通信用，和NetCode RPC关联用
     [XLua.LuaCallCSharp]
     public class MoePlayerController : NetworkBehaviour
