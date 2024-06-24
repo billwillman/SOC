0a6f31e9603535a660d9c6611cb806e2d3efb05c
billwillman <billwillman@qq.com>
2024-06-25 02:03:08 +0800

    12321213

--- Assets/Script/GamePlay/UIBinder.cs
+++ Assets/Script/GamePlay/UIBinder.cs
@@ -13,7 +13,6 @@ namespace SOC.GamePlay
     {
         public UIBehaviour[] m_BindControls = null;
         private ILuaBinder m_Lua = null;
-        private Dictionary<string, UIBehaviour> m_BindControlsMap = null;
 
         private void Awake()
         {
@@ -24,43 +23,16 @@ namespace SOC.GamePlay
         {
             if (GameStart.EnvLua != null && m_BindControls != null && m_Lua != null && m_Lua.LuaSelf != null)
             {
-                m_BindControlsMap = new Dictionary<string, UIBehaviour>(m_BindControls.Length);
-                for (int i = 0; i < m_BindControls.Length; ++i)
-                {
-                    var control = m_BindControls[i];
-                    if (control != null)
-                    {
-                        InitRegisterControl(control);
+                LuaTable bp = m_Lua.LuaSelf.Get<LuaTable>("bp");
+                if (bp != null) {
+                    for (int i = 0; i < m_BindControls.Length; ++i) {
+                        var control = m_BindControls[i];
+                        if (control != null) {
+                            bp.Set<string, UIBehaviour>(control.gameObject.name, control);
+                        }
                     }
-                    m_Lua.LuaSelf.SetInPath<Dictionary<string, UIBehaviour>>("bp", m_BindControlsMap);
                 }
             }
-        }
-
-        private bool InitRegisterControl(UIBehaviour control)
-        {
-            if (control == null)
-                return false;
-            string name = control.gameObject.name;
-            m_BindControlsMap[name] = control;
-            return true;
-        }
-
-        public bool UnRegisterControl(UIBehaviour control)
-        {
-            if (m_BindControlsMap == null)
-                return false;
-            string name = control.gameObject.name;
-            UIBehaviour findControl;
-            if (m_BindControlsMap.TryGetValue(name, out findControl))
-            {
-                if (findControl == control)
-                {
-                    m_BindControlsMap.Remove(name);
-                    return true;
-                }
-            }
-            return false;
         }
 
         private void Start()
