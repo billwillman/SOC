ba0c48e8e48be2d94bd745e054d523e8805582c5
billwillman <billwillman@qq.com>
2024-07-08 01:19:11 +0800

    2222

--- Assets/Script/GamePlay/Editor/AnimToCurve/AnimToCurveWindow.cs
+++ Assets/Script/GamePlay/Editor/AnimToCurve/AnimToCurveWindow.cs
@@ -1,27 +1,26 @@
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements.Experimental;
 
 public class AnimToCurveWindow : EditorWindow
 {
     [SerializeField]
     private VisualTreeAsset m_VisualTreeAsset = default;
 
-    [MenuItem("Window/UI Toolkit/AnimToCurveWindow")]
+    [MenuItem("Tools/动画转曲线")]
     public static void ShowExample()
     {
         AnimToCurveWindow wnd = GetWindow<AnimToCurveWindow>();
-        wnd.titleContent = new GUIContent("AnimToCurveWindow");
+        wnd.titleContent = new GUIContent("动画转曲线");
     }
 
     public void CreateGUI()
     {
         // Each editor window contains a root VisualElement object
         VisualElement root = rootVisualElement;
-
-        // VisualElements objects can contain other VisualElement following a tree hierarchy.
-        VisualElement label = new Label("Hello World! From C#");
-        root.Add(label);
 
         // Instantiate UXML
         VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
---
+++ Assets/Script/GamePlay/Editor/EditorUI/ElementObjectField.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements.Experimental;
+
+// 物件目标
+[UxmlElement]
+public partial class ElementObjectField: ObjectField
+{}
