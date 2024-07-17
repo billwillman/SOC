20dce0ec7fb42daed36255dc94f3144a504c336d
billwillman <billwillman@qq.com>
2024-07-17 10:07:14 +0800

    3333

--- MakeDirLink.bat
+++ MakeDirLink.bat
@@ -1,5 +1,7 @@
 @echo off
 echo CurrentDir: %CD%
 mklink /d "%CD%/Assets/Resources/@Lua/_Common" "%CD%/Common/Lua/_Common"
-mklink /d "%CD%/Server/moon/Lua/_Common" "%CD%/Common/Lua/_Common" 
+mklink /d "%CD%/Server/moon/Lua/_Common" "%CD%/Common/Lua/_Common"
+cd %CD%/Server/moon/Lua/_Common
+xcopy /Y ".\*.lua.bytes" ".\*.lua"
 pause
