b04855fac11bc6070de44b80f2d9d217b41773b6
billwillman <billwillman@qq.com>
2024-07-17 09:42:46 +0800

    1111

--- Server/moon/Lua/LoginServer.lua
+++ Server/moon/Lua/LoginServer.lua
@@ -13,7 +13,7 @@ serverCfgStr = nil
 local listenfd = socket.listen(ServerData.ip, ServerData.port, moon.PTYPE_SOCKET_MOON)
 socket.start(listenfd)--auto accept
 
-local TableUtils = require("Common.TableUtils")
+local TableUtils = require("_Common.TableUtils")
 
 --注册网络事件
 socket.on("accept",function(fd, msg)
