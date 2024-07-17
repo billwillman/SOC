58a1a6ec31521feb967f7e17ce761e2eacfabddf
bestzeng <bestzeng@tencent.com>
2024-07-17 17:23:21 +0800

    111

--- MakeDirLink.bat
+++ MakeDirLink.bat
@@ -2,13 +2,16 @@
 echo CurrentDir: %~dp0
 mklink /d "%~dp0/Assets/Resources/@Lua/_Common" "%~dp0/Common/Lua/_Common"
 mklink /d "%~dp0/Server/moon/Lua/_Common" "%~dp0/Common/Lua/_Common"
+mklink /d "%~dp0/Assets/Resources/@Lua/_NetMsg" "%~dp0/Common/Lua/_NetMsg"
+mklink /d "%~dp0/Server/moon/Lua/_NetMsg" "%~dp0/Common/Lua/_NetMsg"
+
+
 cd %~dp0/Server/moon/Lua/_Common
 xcopy /Y ".\*.lua.bytes" ".\*.lua"
 ren "*.lua" "*."
 
-mklink /d "%~dp0/Assets/Resources/@Lua/_NetMsg" "%~dp0/Common/Lua/_NetMsg"
-mklink /d "%~dp0/Server/moon/Lua/_NetMsg" "%~dp0/Common/Lua/_NetMsg"
 cd %~dp0/Server/moon/Lua/_NetMsg
+
 xcopy /Y ".\*.lua.bytes" ".\*.lua"
 ren "*.lua" "*."
 pause
