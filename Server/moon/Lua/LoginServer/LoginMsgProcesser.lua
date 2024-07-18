5c693b34f19bc7b65d4dc25e1b0b303dfeee19a1
billwillman <billwillman@qq.com>
2024-07-19 02:04:52 +0800

    222222

--- Server/moon/Lua/LoginServer/LoginMsgProcesser.lua
+++ Server/moon/Lua/LoginServer/LoginMsgProcesser.lua
@@ -1,9 +1,19 @@
 local _M = _MOE.class("LoginMsgProcesser")
 
 local TableUtils = require("_Common.TableUtils")
+local MsgIds = require("_NetMsg.MsgId")
+
+_M.MsgDispatch = {
+    [MsgIds.CM_Heart] = function (msg)
+    end
+}
 
 function _M:OnMsg(msg)
     print(TableUtils.Serialize(msg))
+    local func = _M.MsgDispatch[msg.MsgId]
+    if func then
+        func(msg)
+    end
 end
 
 return _M
