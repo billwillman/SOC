e6790650bc81d381326f48b39be57ce88c08cd03
billwillman <billwillman@qq.com>
2024-07-21 03:45:27 +0800

    111

--- Server/moon/Lua/InitGlobalVars.lua
+++ Server/moon/Lua/InitGlobalVars.lua
@@ -4,6 +4,6 @@ moon.exports._MOE = _MOE
 _MOE.class = require("_Common.BaseClass")
 
 _MOE.ErrorHandler = function (err)
-    _MOE.Logger.LogError(err)
-	_MOE.Logger.LogError(debug.traceback())
+    print(err)
+	print(debug.traceback())
 end
