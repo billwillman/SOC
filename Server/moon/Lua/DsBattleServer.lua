db58ab2d270850e1a4f39ac0ecc9a38e7fb561bf
billwillman <billwillman@qq.com>
2024-07-29 01:41:25 +0800

    222

--- Server/moon/Lua/DsBattleServer.lua
+++ Server/moon/Lua/DsBattleServer.lua
@@ -1,2 +1,30 @@
 -- Ds连接到GS
 require("LuaPanda").start("127.0.0.1", 20002)
+
+local json = require("json")
+local moon = require("moon")
+local socket = require "moon.socket"
+require("InitGlobalVars")
+
+local MsgProcesser = require("LoginServer/LoginMsgProcesser").New()
+
+local serverCfgStr = io.readfile("./Config/Server.json")
+local serverCfg = json.decode(serverCfgStr)
+local ServerData = serverCfg.BattleSrv -- 战斗服务器
+serverCfg = nil
+serverCfgStr = nil
+
+local listenfd = socket.listen(ServerData.ip, ServerData.port, moon.PTYPE_SOCKET_MOON)
+socket.start(listenfd)--auto accept
+
+--注册网络事件
+socket.on("accept",function(fd, msg)
+    print("accept ", fd, moon.decode(msg, "Z"))
+    socket.settimeout(fd, 10)
+    --socket.setnodelay(fd)
+    --socket.set_enable_chunked(fd, "w")
+end)
+
+socket.on("close", function(fd, msg)
+    print("close ", fd, moon.decode(msg, "Z"))
+end)
