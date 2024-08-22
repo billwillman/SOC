f4c3cb9558177634f94dab75e269bb1711b3cb8c
billwillman <billwillman@qq.com>
2024-08-23 01:48:44 +0800

    1111

--- Server/moon/Lua/CommonMsgProcesser.lua
+++ Server/moon/Lua/CommonMsgProcesser.lua
@@ -1,15 +1,28 @@
 local _M = _MOE.class("CommonMsgProcesser")
+local MsgIds = require("_NetMsg.MsgId")
+local json = require("json")
+local moon = require("moon")
 
 _M.MsgDispatch = {
-    [MsgIds.CM_Heart] = function (msg, socket, fd)
+    [MsgIds.CM_Heart] = function (self, msg, socket, fd)
+        local sendMsg = {MsgId = MsgIds.SM_Heart}
+        self:SendTableToJson(socket, fd, sendMsg)
     end
 }
+
+function _M:SendTableToJson(socket, fd, sendMsg)
+    if not socket or not fd or not sendMsg or not sendMsg.MsgId then
+        return false
+    end
+    local str = json.encode(sendMsg)
+    socket.write(fd, string.pack(">H",#str)..str)
+end
 
 function _M:OnMsg(msg, socket, fd)
     print(_MOE.TableUtils.Serialize(msg))
     local func = _M.MsgDispatch[msg.MsgId]
     if func then
-        func(msg, socket, fd)
+        func(self, msg, socket, fd)
         return true
     end
     return false
--- Server/moon/Lua/DsBattleServer/DsBattleServerProcesser.lua
+++ Server/moon/Lua/DsBattleServer/DsBattleServerProcesser.lua
@@ -1,7 +1,9 @@
 local baseClass = require("CommonMsgProcesser")
 local _M = _MOE.class("DsBattleServerProcesser", baseClass)
 
+local json = require("json")
 local MsgIds = require("_NetMsg.MsgId")
+local moon = require("moon")
 
 local CurrentMsgProcess = {
 
