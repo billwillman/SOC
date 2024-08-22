e3a89e53a65659e2a574dfad01a965d2313aba81
bestzeng <bestzeng@tencent.com>
2024-08-23 00:25:58 +0800

    2222

--- Server/moon/Lua/DsBattleServer/DsBattleServerProcesser.lua
+++ Server/moon/Lua/DsBattleServer/DsBattleServerProcesser.lua
@@ -3,4 +3,10 @@ local _M = _MOE.class("DsBattleServerProcesser", baseClass)
 
 local MsgIds = require("_NetMsg.MsgId")
 
+local CurrentMsgProcess = {
+
+}
+
+setmetatable(_M.MsgDispatch, {__index = CurrentMsgProcess})
+
 return _M
--- Server/moon/Lua/LoginServer/LoginMsgProcesser.lua
+++ Server/moon/Lua/LoginServer/LoginMsgProcesser.lua
@@ -3,4 +3,10 @@ local _M = _MOE.class("LoginMsgProcesser", baseClass)
 
 local MsgIds = require("_NetMsg.MsgId")
 
+local CurrentMsgProcess = {
+
+}
+
+setmetatable(_M.MsgDispatch, {__index = CurrentMsgProcess})
+
 return _M
