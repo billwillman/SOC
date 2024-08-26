bb3e85a4e32e17983c9594726825512ca9be73f8
bestzeng <bestzeng@tencent.com>
2024-08-27 00:25:17 +0800

    2222

--- Server/moon/Lua/LoginServer/LoginMsgProcesser.lua
+++ Server/moon/Lua/LoginServer/LoginMsgProcesser.lua
@@ -4,6 +4,7 @@ local _M = _MOE.class("LoginMsgProcesser", baseClass)
 local json = require("json")
 local MsgIds = require("_NetMsg.MsgId")
 local moon = require("moon")
+require("ServerCommon.ServerMsgIds")
 
 local CurrentMsgProcess = {
     -- 登录协议
