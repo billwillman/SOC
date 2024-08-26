b3b6a20f4a283c3f260767295e3f42461280621b
bestzeng <bestzeng@tencent.com>
2024-08-27 00:42:42 +0800

    33333

--- Server/moon/Lua/LoginServer/LoginMsgProcesser.lua
+++ Server/moon/Lua/LoginServer/LoginMsgProcesser.lua
@@ -6,19 +6,33 @@ local MsgIds = require("_NetMsg.MsgId")
 local moon = require("moon")
 require("ServerCommon.ServerMsgIds")
 
+local DSA_ServerId = nil
+
+function GetDSAServerId()
+    if DSA_ServerId then
+        return DSA_ServerId
+    end
+    DSA_ServerId = moon.queryservice("DSA")
+    return DSA_ServerId
+end
+
 local CurrentMsgProcess = {
     -- 登录协议
     [MsgIds.CM_Login] = function (self, msg, socket, fd)
-        local s = string.format("%s+%s+%d", msg.userName, msg.password, os.time())
-        local ret = {
-            -- 暂时这样，后面采用DSA分配拉起模式
-            DsServer = {
-                ip = "127.0.0.1",
-                port = 7777
-            },
-            token = moon.md5(s) -- token 
-        }
-        self:SendTableToJson(socket, fd, ret)
+        local dsa = GetDSAServerId()
+        if dsa then
+            moon.send("lua", dsa, _MOE.ServerMsgIds.CM_ReqDS) -- 从DSA请求服务器
+            local s = string.format("%s+%s+%d", msg.userName, msg.password, os.time())
+            local ret = {
+                -- 暂时这样，后面采用DSA分配拉起模式
+                DsServer = {
+                    ip = "127.0.0.1",
+                    port = 7777
+                },
+                token = moon.md5(s) -- token 
+            }
+            self:SendTableToJson(socket, fd, ret)
+        end
     end
 }
 
---
+++ Server/moon/Lua/ServerCommon/ServerMsgIds.lua
@@ -0,0 +1,4 @@
+_MOE = _MOE or {}
+_MOE.ServerMsgIds = {
+    CM_ReqDS = 1, -- 请求一个新的DS服务器
+}
