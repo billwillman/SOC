c095eeb55bbaa30ea4402853bbc76dddaa4168ec
billwillman <billwillman@qq.com>
2024-09-18 02:22:49 +0800

    111

--- Server/moon/Lua/DSAServer/DSManager.lua
+++ Server/moon/Lua/DSAServer/DSManager.lua
@@ -39,7 +39,7 @@ function _M:StartDSAsync(playerInfos)
     }
     local jsonStr = json.encode(dsParam)
     print("[DSA] Command: " .. jsonStr)
-    local handler = io.popen(string.format("start \"../../SOC/SOC.exe.lnk %s\" ", jsonStr), "r")
+    local handler = io.popen(string.format("\"../../SOC/SOC.exe.lnk %s\" ", jsonStr), "r")
     if not handler then
         print("[dsa] not run SOC.exe...")
         return
