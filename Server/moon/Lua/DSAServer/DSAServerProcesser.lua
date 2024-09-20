c7a3ee331b835af5ac5ee87e331eadbd113b1e20
bestzeng <bestzeng@tencent.com>
2024-09-20 20:23:41 +0800

    222

--- Server/moon/Lua/DSAServer/DSAServerProcesser.lua
+++ Server/moon/Lua/DSAServer/DSAServerProcesser.lua
@@ -10,4 +10,62 @@ local CurrentMsgProcess = {
 
 setmetatable(_M.MsgDispatch, {__index = CurrentMsgProcess})
 
+-------------------------------------------- 服务器之间通信 ------------------------------------------
+
+-- 跨服务器处理
+local _Server_DSA_Process = {
+    [_MOE.ServerMsgIds.CM_ReqDS] = function (playerInfos)
+        -- 拉起DS
+        -- print("[DSA] PlayerInfo:" .. TableUtils.Serialize(playerInfo))
+        if type(playerInfos) == "table" then
+            if playerInfos[1] == nil and next(playerInfos) ~= nil then
+                -- 说明是数组
+                local arr = {}
+                table.insert(arr, playerInfos)
+                playerInfos = arr
+            end
+            -- StartDSAsync(playerInfos)
+            local dsData = DSManager:StartDSAsync(playerInfos)
+            if dsData then
+                -- 返回等待登录成功
+                local msg = {
+                    playerInfos = playerInfos,
+                    dsData = {
+                        dsStatus = _MOE.DsStatus.WaitRunning,
+                        dsToken = dsData.token,
+                        dsIp = dsData.dsIp,
+                        dsPort = dsData.dsPort
+                    }
+                }
+                if not SendLoginServer(_MOE.ServerMsgIds.SM_DS_STATUS, msg) then
+                    print("[DSA] SendLoginServer Error: " .. msg.dsData.dsStatus)
+                    DSManager:StopDS(dsData.dsToken) -- 主动关闭
+                end
+            else
+                -- 返回失败状态
+                local msg = {
+                    playerInfos = playerInfos,
+                    dsData = {
+                        dsStatus = _MOE.DsStatus.StartError
+                    }
+                }
+                if not SendLoginServer(_MOE.ServerMsgIds.SM_DS_STATUS, msg) then
+                    print("[DSA] SendLoginServer Error: " .. msg.dsStatus)
+                end
+            end
+        end
+    end,
+    -- DS准备好
+    [_MOE.ServerMsgIds.SM_DSReady] = function ()
+    end
+}
+
+moon.dispatch("lua", function(_, _, cmd, ...)
+    -- 处理 cmd
+    local OnProcess = _Server_DSA_Process[cmd]
+    if OnProcess then
+        OnProcess(...)
+    end
+end)
+
 return _M
