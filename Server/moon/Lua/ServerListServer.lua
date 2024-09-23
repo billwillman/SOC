97c7d6bf7dcfc394d63ba302ee81d94352395146
billwillman <billwillman@qq.com>
2024-09-24 03:07:27 +0800

    2222

--- Server/moon/Lua/ServerListServer.lua
+++ Server/moon/Lua/ServerListServer.lua
@@ -20,22 +20,21 @@ httpserver.header_max_len = 8192
 local G_StartTimer = os.clock() * 1000
 
 
-httpserver.error = function (fd, err)
-    print("http server fd",fd," disconnected:",  err)
-end
-
-httpserver.on("/serverlist", function(req, rep)
-    -- 返回区服数据
-    rep.status_code = 200
-    rep:write(clientServerListStr)
-end)
-
 ---------------------------------------------- 服务之间通信 ------------------------------------------
 
 local _Server_List_Process = {
     [_MOE.ServicesCall.Start] = function ()
         httpserver.listen(ServerData.ip, ServerData.port, 60)
         print("[ServerListServer] ", ServerData.ip, ServerData.port)
+        httpserver.error = function (fd, err)
+            print("http server fd",fd," disconnected:",  err)
+        end
+        
+        httpserver.on("/serverlist", function(req, rep)
+            -- 返回区服数据
+            rep.status_code = 200
+            rep:write(clientServerListStr)
+        end)
     end,
     [_MOE.ServicesCall.Shutdown] = function ()
     end
