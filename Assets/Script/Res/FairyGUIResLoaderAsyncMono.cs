5a23558d3a3be4968f60cdbdf9fcff67b1717c97
billwillman <billwillman@qq.com>
2024-09-30 02:46:20 +0800

    2222

---
+++ Assets/Script/Res/FairyGUIResLoaderAsyncMono.cs
@@ -0,0 +1,16 @@
+﻿#define _USE_FAIRY_GUI
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#if _USE_FAIRY_GUI
+
+[XLua.LuaCallCSharp]
+public class FairyGUIResLoaderAsyncMono: BaseResLoaderAsyncMono {
+
+}
+
+#endif
