700e672385b9b13ad4a6c7e408cf975a595392c4
billwillman <billwillman@qq.com>
2024-10-06 00:23:15 +0800

    12312321

--- Assets/Script/GamePlay/PlayerController.cs
+++ Assets/Script/GamePlay/PlayerController.cs
@@ -4,5 +4,15 @@ using Unity.Netcode;
 namespace SOC.GamePlay
 {
     public class PlayerController : NetworkBehaviour
-    {}
+    {
+        // Client和Server都会执行
+        public override void OnNetworkSpawn() {
+            base.OnNetworkSpawn();
+        }
+
+        // Client和Server都会执行
+        public override void OnNetworkDespawn() {
+            base.OnNetworkDespawn();
+        }
+    }
 }
