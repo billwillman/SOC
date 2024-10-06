dbc99c1aaff6f12441d379e1587976092bf93b84
billwillman <billwillman@qq.com>
2024-10-07 00:20:05 +0800

    222

--- Assets/Script/GamePlay/Player/MoePlayerController.cs
+++ Assets/Script/GamePlay/Player/MoePlayerController.cs
@@ -8,7 +8,7 @@ namespace SOC.GamePlay.Player
     // 加上这个可以在LUA覆写方法
     // 用来做和DS做通信用，和NetCode RPC关联用
     [XLua.LuaCallCSharp]
-    public class MoePlayerController : NetworkBehaviour
+    public class MoePlayerController : PlayerController
     {
         public MoeCharacterController Character;
     }
