10c47252971ede47a5a9ad413d76039aca73f355
billwillman <billwillman@qq.com>
2024-10-07 15:19:18 +0800

    2222

--- Assets/Script/GamePlay/PlayerController.cs
+++ Assets/Script/GamePlay/PlayerController.cs
@@ -3,14 +3,17 @@ using Unity.Netcode;
 
 namespace SOC.GamePlay
 {
+    [XLua.LuaCallCSharp]
     public class PlayerController : NetworkBehaviour
     {
         // Client和Server都会执行
+        [XLua.DoNotGen]
         public override void OnNetworkSpawn() {
             base.OnNetworkSpawn();
         }
 
         // Client和Server都会执行
+        [XLua.DoNotGen]
         public override void OnNetworkDespawn() {
             base.OnNetworkDespawn();
         }
