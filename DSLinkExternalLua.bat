7fc095641baae1c950da728ad8d06a54b305dc5a
bestzeng <bestzeng@tencent.com>
2024-10-08 10:52:37 +0800

    2222

--- DSLinkExternalLua.bat
+++ DSLinkExternalLua.bat
@@ -1,4 +1,5 @@
 @echo off
 echo CurrentDir: %~dp0
 mklink /d "%~dp0/outPath/DS/Server_Data/Lua" "%~dp0/Assets/Resources/@Lua"
+mklink /d "%~dp0/outPath/Win_Build/client_Data/Lua" "%~dp0/Assets/Resources/@Lua"
 pause
