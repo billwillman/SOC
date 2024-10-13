e0f98bd130b4cc92c6aec5db859c441b3b5841dc
billwillman <billwillman@qq.com>
2024-10-13 22:19:52 +0800

    11111

--- MakeDirLink.bat
+++ MakeDirLink.bat
@@ -6,6 +6,7 @@ mklink /d "%~dp0/Assets/Resources/@Lua/_NetMsg" "%~dp0/Common/Lua/_NetMsg"
 mklink /d "%~dp0/Server/moon/Lua/_NetMsg" "%~dp0/Common/Lua/_NetMsg"
 mklink /d "%~dp0/Server/DS" "%~dp0/outPath/DS"
 mklink /d "%~dp0/Library/PackageCache/com.unity.netcode.gameobjects" "%~dp0/backup/com.unity.netcode.gameobjects"
+mklink /d "%~dp0/outPath/Proj/Library/PackageCache/com.unity.netcode.gameobjects" "%~dp0/backup/com.unity.netcode.gameobjects"
 
 cd %~dp0/Server/moon/Lua/_Common
 del ".\*.lua"
