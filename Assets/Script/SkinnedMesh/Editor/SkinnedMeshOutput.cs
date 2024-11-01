99a39ca813c33cf70b81f407a4a1edfa122fab3e
billwillman <billwillman@qq.com>
2024-11-02 02:20:24 +0800

    22222

---
+++ Assets/Script/SkinnedMesh/Editor/SkinnedMeshOutput.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEditor;
+
+public class SkinnedMeshOutput: Editor
+{
+    [MenuItem("Assets/SkinnedMesh/导出AI-FBX格式", validate = true)]
+    public static bool IsCanOutput() {
+        var gameObj = Selection.activeGameObject;
+        if (!gameObj)
+            return false;
+        if (gameObj.scene.IsValid())
+            return false;
+        SkinnedMeshRenderer skl = gameObj.GetComponentInChildren<SkinnedMeshRenderer>();
+        return skl != null;
+    }
+    [MenuItem("Assets/SkinnedMesh/导出AI-FBX格式")]
+    public static void Output() {
+
+    }
+}
