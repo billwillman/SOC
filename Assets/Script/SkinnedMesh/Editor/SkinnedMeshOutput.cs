8bb7278c233b229777fa343f9eeefb2bdb9d3b8c
billwillman <billwillman@qq.com>
2024-11-04 02:23:25 +0800

    111

--- Assets/Script/SkinnedMesh/Editor/SkinnedMeshOutput.cs
+++ Assets/Script/SkinnedMesh/Editor/SkinnedMeshOutput.cs
@@ -68,9 +68,9 @@ public class SkinnedMeshOutput: Editor
         AddBoneToList(bones, rootNode);
         
         string name = gameObj.name;
-        ExportPosition(dir, name, bones);
-        ExportRotation(dir, name, bones);
-        ExportScale(dir, name, bones);
+        ExportPosition(dir, name, bones, false);
+        ExportRotation(dir, name, bones, false);
+        ExportScale(dir, name, bones, false);
         ExportBoneNames(dir, name, bones);
         ExportBoneLink(dir, name, bones);
         ExportBoneVertexWeight(dir, name, bones, skl);
