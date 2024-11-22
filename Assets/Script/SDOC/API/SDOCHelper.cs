6092e0f352658219c92a99826bde57ff6206b5ee
billwillman <billwillman@qq.com>
2024-11-23 02:46:43 +0800

    2222

---
+++ Assets/Script/SDOC/API/SDOCHelper.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace SDOC
+{
+
+    public static unsafe class SDOCHelper
+    {
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+        [DllImport("libSDOC.quic")]
+        public static extern void* sdocInit(uint width, uint height, float nearPlane);
+        [DllImport("libSDOC.quic")]
+        public static extern bool sdocStartNewFrame(void* pSDOC, float* ViewPos, float* ViewDir, float* ViewProj);
+        [DllImport("libSDOC.quic")]
+        public static extern void sdocRenderOccluder(void* pSDOC, float* vertices, ushort* indices, uint nVert, uint nIdx, float* localToWorld, bool enableBackfaceCull);
+#endif
+    }
+}
