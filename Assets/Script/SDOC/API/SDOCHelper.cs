c76029c0a2da37869c47aa819fa5d71202b23951
bestzeng <bestzeng@tencent.com>
2024-11-23 14:55:21 +0800

    2222

--- Assets/Script/SDOC/API/SDOCHelper.cs
+++ Assets/Script/SDOC/API/SDOCHelper.cs
@@ -15,7 +15,21 @@ namespace SDOC
         [DllImport("libSDOC.quic")]
         public static extern void sdocRenderOccluder(void* pSDOC, float* vertices, ushort* indices, uint nVert, uint nIdx, float* localToWorld, bool enableBackfaceCull);
         [DllImport("libSDOC.quic")]
+        public static extern bool sdocQueryOccludeeMesh(void* pSDOC, float* vertices, ushort* indices, uint nVert, uint nIdx, float* localToWorld, bool enableBackfaceCull, float* worldAABB);
+        [DllImport("libSDOC.quic")]
+        public static extern bool sdocQueryOccludees(void* pSDOC, float* bbox, uint nMesh, bool* results);
+        [DllImport("libSDOC.quic")]
+        public static extern bool sdocQueryOccludees_OBB(void* pSDOC, float* bbox, uint nMesh, bool* results);
+        [DllImport("libSDOC.quic")]
         public static extern bool sdocSet(void* pSDOC, uint ID, uint configValue);
+        [DllImport("libSDOC.quic")]
+        public static extern bool sdocSync(void * pSDOC, uint id, void *param);
+        [DllImport("libSDOC.quic")]
+        public static extern ushort* sdocMeshBake(int* outputCompressSize, float *vertices, ushort *indices, uint nVert, uint nIdx,  float quadAngle, bool enableBackfaceCull, bool counterClockWise, int SquareTerrainAxisPoints);
+        [DllImport("libSDOC.quic")]
+        public static extern bool sdocMeshLod(float* vertices, ushort* indices, uint& nVert, uint& nIdx, int modelId, uint targetFaceNum, bool saveModel);
+        [DllImport("libSDOC.quic")]
+        void sdocRenderBakedOccluder(void * pSDOC, ushort *compressedModel, float *localToWorld);
 #endif
     }
 }
