c9a78f972d7fd13c187146890089be8fdb7cd5fc
billwillman <billwillman@qq.com>
2024-11-24 02:45:43 +0800

    22222

---
+++ Assets/Script/SDOC/SDOCMeshOccluder.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace SDOC
+{
+    public unsafe class SDOCMeshOccluder : MonoBehaviour
+    {
+        public Mesh m_TargetMesh = null;
+        private ushort* m_OccluderMesh = null;
+
+        protected void DestroyOccluderMesh() {
+            if (m_OccluderMesh == null)
+                return;
+            m_OccluderMesh = SDOCHelper.sdocMeshBake((int*)m_OccluderMesh, null, null, 0, 0, 0, false, false, 0);
+        }
+
+        private void OnApplicationQuit() {
+            DestroyOccluderMesh();
+        }
+
+        private void OnDestroy() {
+            DestroyOccluderMesh();
+        }
+    }
+}
