75bffdbbe169407e0fdfb3c8bbf8a0ed2ab71c01
billwillman <billwillman@qq.com>
2024-12-04 03:05:47 +0800

    1111

--- Assets/Script/SDOC/SDOCMeshOccluder.cs
+++ Assets/Script/SDOC/SDOCMeshOccluder.cs
@@ -4,12 +4,11 @@ namespace SDOC
 {
     public unsafe class SDOCMeshOccluder : MonoBehaviour
     {
-        public SDOCMeshData m_TargetMesh = null;
-        
+        private SDOCMeshData m_TargetMesh = null;
+
         public void SDOCRender(void* pSDOCInstance) {
-            if (pSDOCInstance == null || m_TargetMesh == null || !m_TargetMesh.IsVaildData())
+            if (pSDOCInstance == null || ((m_TargetMesh == null || !m_TargetMesh.IsVaildData())))
                 return;
-            
             ushort* pOccluderMesh = m_TargetMesh.ReadOnlyDataPtr;
             if (pOccluderMesh != null) {
                 Matrix4x4 mat = transform.localToWorldMatrix;
